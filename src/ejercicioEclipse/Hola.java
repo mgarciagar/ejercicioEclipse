5fb3866099a1b6ebfd6d5e4cfee3a2099f8cf5b2
Mario Garcia <mgarciaga@viewnext.com>
2018-02-08 19:12:51 +0100

    Version 3

--- src/ejercicioEclipse/Hola.java
+++ src/ejercicioEclipse/Hola.java
@@ -4,6 +4,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("¿Qué tal?");
-		System.out.println("¿Me has escuchado?");
+		System.out.println("Muy bien");
 	}
 }
